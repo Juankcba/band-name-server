9e716257f50c1255d6384ef6f5edac95da518245
juankcba <juangblade@hotmail.com>
2023-05-18 19:27:46 -0300

    cambios

--- models/server.js
+++ models/server.js
@@ -1,49 +1,46 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
-const socketio = require('socket.io');
-const path     = require('path');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
 
-const Sockets  = require('./sockets');
+const Sockets = require("./sockets");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = 8080;
 
-    constructor() {
+    // Http server
+    this.server = http.createServer(this.app);
 
-        this.app  = express();
-        this.port = process.env.PORT;
+    // Configuraciones de sockets
+    this.io = socketio(this.server, {
+      /* configuraciones */
+    });
+  }
 
-        // Http server
-        this.server = http.createServer( this.app );
-        
-        // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
-    }
+  middlewares() {
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
 
-    middlewares() {
-        // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-    }
+  configurarSockets() {
+    new Sockets(this.io);
+  }
 
-    configurarSockets() {
-        new Sockets( this.io );
-    }
+  execute() {
+    // Inicializar Middlewares
+    this.middlewares();
 
-    execute() {
+    // Inicializar sockets
+    this.configurarSockets();
 
-        // Inicializar Middlewares
-        this.middlewares();
-
-        // Inicializar sockets
-        this.configurarSockets();
-
-        // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
-        });
-    }
-
+    // Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto:", this.port);
+    });
+  }
 }
-
 
 module.exports = Server;
